960da45660e6be0c73a7efdff133364f1ba9f4da
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2022-11-04 23:09:39 +0100

    discorso non essenziale

--- JS/main.js
+++ JS/main.js
@@ -4,51 +4,6 @@
 // poi chiedi il suo colore preferito
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
-// let miaStringa;
-// let miaStringa1;
-// let miaStringa2;
-// let miaStringa3;
-
-// // alert(miaStringa);
-
-
-// miaStringa = 'Arrivederci';
-// console.log(miaStringa);
-
-
-// miaStringa = 5;
-// console.log(miaStringa);
-
-
-// miaStringa = "5" + " " + "sei";
-// console.log(miaStringa);
-
-
-// miaStringa1 = "mia1";
-// miaStringa = "5" + " " + "sei" + " " + miaStringa1;
-// console.log(miaStringa);
-
-
-// miaStringa1 = "mia1";
-
-
-// miaStringa3 = 
-// `fh3r9uhec cuhwc9uhre9c+diehbcc
-// ${miaStringa}
-// eicuberciuh rceruh jjnrfij
-// `;
-// // `
-// // `
-// console.log(miaStringa3);
-
-// const nome = prompt("inserisci il tuo nome");
-// console.log(nome);
-// text = parseInt(prompt("inserisci il tuo numero"));
-// console.log(text);
-
-
-// document.getElementById("titolo").innerHTML = "ciao " + nome;
-
 
 const numero = 21;
 
@@ -70,7 +25,18 @@ let password = nome + cognome + colPref + numero;
 
 
 document.getElementById("password").innerHTML = 
-`La tua password è:  ${password}
-`
-;
+`${password}
+`;
+
+document.getElementById("nome").innerHTML = 
+`${nome}
+`;
+
+document.getElementById("cognome").innerHTML = 
+`${cognome}
+`;
+
+document.getElementById("colPref").innerHTML = 
+`${colPref}
+`;
 
